a68879d76a12e972786df61f288f949735e63f33
minoruzxc <minoruzxc@gmail.com>
2025-05-23 13:58:39 -0300

    implementado a listagem da tabela do banco MySQL

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -10,8 +10,10 @@
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -19,19 +21,35 @@ public class ProdutosDAO {
     
     Connection conn;
     PreparedStatement prep;
-    ResultSet resultset;
+    Statement st;
+    ResultSet rs;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try{
+            conn = new conectaDAO().connectDB();
+            st = conn.createStatement();
+            
+        }catch(Exception e){
+            System.out.println("Erro ao cadastrar produto.");
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
+        try{
+            conn = new conectaDAO().connectDB();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from produtos");
+            while(rs.next()){
+                    ProdutosDTO p = new ProdutosDTO();
+                    p.setNome(rs.getString("nome"));
+                    p.setValor(rs.getInt("valor"));
+                    p.setStatus(rs.getString("status"));
+                    p.setId(rs.getInt("id"));
+                    listagem.add(p);
+                    System.out.println("added "+ rs.getString("nome"));
+            }
+        }catch(SQLException e){System.out.println("SQL exception occured: "+e.getMessage());};
         return listagem;
     }
     
