fa7ac5950ffd1ab6cc09905658f5105cc2dbe44b
minoruzxc <minoruzxc@gmail.com>
2025-05-23 12:33:10 -0300

    classe main adicionada e tela principal definida.

---
+++ LeiloesTDSat/src/LeiloesTDSat.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Misu
+ */
+public class LeiloesTDSat {
+    public static void main(String[] args) {
+        cadastroVIEW cv = new cadastroVIEW();
+        cv.setVisible(true);
+    }
+}
